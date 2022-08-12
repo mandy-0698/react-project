e03c3453f9ec2259ec81465cdd9a8ceb2c625a50
Mritunjay Yadav <binshuyadav123@gmail.com>
2022-08-12 10:28:05 +0530

    validating username and age

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -13,6 +13,12 @@ const AddUser=(props)=>{
     }
     const addUserHandler=(event)=>{
         event.preventDefault();
+        if(enteredUserName.trim().length===0 || enteredAge.trim().length===0){
+            return;
+        }
+        if(+enteredAge<1){
+            return;
+        }
         console.log(enteredUserName,enteredAge);
         setUserName('');
         setAge('');
