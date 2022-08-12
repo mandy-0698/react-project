eea2c30d3742e2ef4c8c6e224e25b76bdf843d88
Mritunjay Yadav <binshuyadav123@gmail.com>
2022-08-12 10:14:58 +0530

    using state to store the value of username and age

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,18 +1,29 @@
 import Card from "../UI/Card";
 import classes from './AddUser.module.css'
 import Button from "../UI/Button";
+import { useState } from "react";
 const AddUser=(props)=>{
-
+    const [enteredUserName,setUserName]=useState('');
+    const[enteredAge,setAge]=useState('');
+    const addUserNameHandler=(event)=>{
+        setUserName(event.target.value);
+    }
+    const addAgeHandler=(event)=>{
+        setAge(event.target.value);
+    }
     const addUserHandler=(event)=>{
         event.preventDefault();
+        console.log(enteredUserName,enteredAge);
+        setUserName('');
+        setAge('');
     }
 return(
     <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" value={enteredUserName} onChange={addUserNameHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" value={enteredAge} onChange={addAgeHandler} />
         <Button type="submit">Add User</Button>
     </form>
     </Card>
